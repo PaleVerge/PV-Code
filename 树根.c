db87e2b1223c3e0512cb29d6218ae9087332f301
PaleVerge <fnme370679@gmail.com>
2025-11-21 00:32:29 +0800

    字符串数根

--- 树根.c
+++ 树根.c
@@ -1,20 +1,33 @@
 #include<stdio.h>
+#include<string.h>
+
 int main(void){
-	int sum,n;
-	while (~scanf("%d", &n)){
-		if(n==0)
+	int final_sum,current_sum;
+	long long initial_sum;
+	char s[1010];
+	while (scanf("%s", s)!=EOF){
+		if(strcmp(s,"0")==0){
 			break;
-		do{
-			sum=0;
-			while(n!=0){
-				sum+=n%10;
-				n=n/10;
+		}
+		initial_sum=0;
+
+		for (int i=0;s[i]!='\0';i++){
+			initial_sum+=s[i]-'0';
+		}
+		final_sum=(int)initial_sum;
+
+		while(final_sum>=10) {
+			current_sum=0;
+			int temp_sum=final_sum;
+
+			while (temp_sum > 0) {
+				current_sum += temp_sum % 10;
+				temp_sum /= 10;
 			}
-			n=sum;
-		}while(sum>=10);
-		printf("%d\n", sum);
+			final_sum=current_sum;
+		}
+		printf("%d\n",final_sum);
 	}
-	
-	
-	
+
+	return 0;
 }
