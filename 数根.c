d4b82dae87ce38bd50ff6252325bdbcdb0428163
PaleVerge <fnme370679@gmail.com>
2025-11-21 00:58:04 +0800

    改名字为数根

---
+++ 数根.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void){
+	int final,current;
+	long long initial;
+	char s[100];
+	while (scanf("%s", s)!=EOF){
+		if(strcmp(s,"0")==0){
+			break;
+		}
+		initial=0;
+
+		for (int i=0;s[i]!='\0';i++){
+			initial+=s[i]-'0';
+		}
+		final=(int)initial;
+
+		while(final>=10) {
+			current=0;
+			int temp=final;
+
+			while (temp > 0) {
+				current += temp % 10;
+				temp /= 10;
+			}
+			final=current;
+		}
+		printf("%d\n",final);
+	}
+
+	return 0;
+}
