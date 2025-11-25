69bda805f014b2d13c88a87c8dcfedd3556611ea
PaleVerge <fnme370679@gmail.com>
2025-11-25 17:42:41 +0800

    加法函数

---
+++ add.c
@@ -0,0 +1,14 @@
+//
+// Created by rint on 2025/11/25.
+//
+#include <stdio.h>
+
+int main(void) {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",add(a,b));
+    return 0;
+}
+int add(int a,int b) {
+    return a+b;
+}
