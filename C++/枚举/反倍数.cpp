3bb54a615cf9eb3153e8825ae465da9e99a05143
PaleVerge <fnme370679@gmail.com>
2026-03-13 00:29:46 +0800

    优化反倍数

--- C++/枚举/反倍数.cpp
+++ C++/枚举/反倍数.cpp
@@ -1,16 +1,16 @@
 #include<bits.stdc++.h>
-
 using namespace std;
 
 int main(){
-    int a,b,c;cin>>a>>b>>c;
-    int ans=0;
-    int n;cin>>n;
+    int n,a,b,c,ans=0;
+    cin>>n>>a>>b>>c;
+    
     for(int i=1;i<=n;i++){
         if(i%a==0||i%b==0||i%c==0){
             ans++;
         }
     }
-    cout<<ans<<endl;
+    
+    cout<<ans<<'\n';
     return 0;
 }
