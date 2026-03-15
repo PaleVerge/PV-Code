9b2837bbc662dfc354596e5747ac6b1e9ee4599a
PaleVerge <fnme370679@gmail.com>
2026-03-15 20:50:36 +0800

    小朋友崇拜圈

---
+++ C++/DFS/小朋友崇拜圈.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=1e5+7;
+
+int n,a[N],dfn[N],idx,mindfn;
+
+int dfs(int x){
+    dfn[x]=++idx;
+    if(dfn[a[x]]){
+        if(dfn[a[x]]>=mindfn){
+            return dfn[x]-dfn[a[x]]+1;
+        }
+        return 0;
+    }
+    return dfs(a[x]); 
+
+}
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        if(!dfn[i]){
+            mindfn=idx+1;
+            ans=max(dfs(i),ans);
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
