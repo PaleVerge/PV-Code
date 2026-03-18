524fba47528d0671046b0cc2ba14ea76712527b4
PaleVerge <fnme370679@gmail.com>
2026-03-18 22:42:43 +0800

    添加注释

--- C++/字符串/判断两个字符串是否为anagrams.cpp
+++ C++/字符串/判断两个字符串是否为anagrams.cpp
@@ -4,32 +4,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
     string s1,s2;
     cin>>s1>>s2;
 
-    if (s1.length()!=s2.length())
-    {
+    if (s1.length()!=s2.length()){ //若长度不等，一定不是
         cout<<"no";
         return 0;
     }
-    for (int i=0;i<s1.length();i++)
-    {
-        if (s2.find(s1[i])!=string::npos)
-        {
+    for (int i=0;i<s1.length();i++){
+        if (s2.find(s1[i])!=string::npos){ //若在s2找到，则在s2中删掉
             s2.erase(s2.find(s1[i]),1);
-        }else
-        {
+        }else{
             cout<<"no"<<endl;
             return 0;
         }
     }
-    if (s2.empty())
-    {
+    if (s2.empty()){ //若删干净了，则是
         cout<<"yes"<<endl;
-    }else
-    {
+    }else{
         cout<<"no"<<endl;
     }
 }
