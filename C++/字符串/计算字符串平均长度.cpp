bd4e9c8f4d2b62315e6607b8593437c8024e2029
PaleVerge <fnme370679@gmail.com>
2026-03-18 22:47:45 +0800

    添加注释

--- C++/字符串/计算字符串平均长度.cpp
+++ C++/字符串/计算字符串平均长度.cpp
@@ -12,17 +12,14 @@ int main()
     double count=0,tot=0;
 
     getline(cin,s);
-    for (int i=0;i<s.length();i++)
-    {
-        if (s[i]==' ' || s[i]==',' || s[i]=='.')
-        {
+    for (int i=0;i<s.length();i++){
+        if (s[i]==' ' || s[i]==',' || s[i]=='.'){   //若当前字符是分隔符，则不在单词中
            inword=false;
-        }else{
-            tot++;
-            if (!inword)
-            {
+        }else{  //若当前字符不是分隔符，则可能在或不在单词中
+            tot++;  //字母总数增加
+            if (!inword){   //若此前不在单词中，则单词数增加
                 count++;
-                inword=true;
+                inword=true;    //现在在单词中了
             }
         }
     }
