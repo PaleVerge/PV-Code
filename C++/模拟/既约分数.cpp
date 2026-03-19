0cfaabaa2d3cdde1c4b5039a84fc832836899cea
PaleVerge <fnme370679@gmail.com>
2026-03-19 23:26:07 +0800

    手写gcd，添加注释

--- C++/模拟/既约分数.cpp
+++ C++/模拟/既约分数.cpp
@@ -5,10 +5,18 @@
 #include <algorithm>
 using namespace std;
 int ans=0;
+
+int gcd(int a,int b){
+    if (b==0){
+        return a;
+    }
+    return gcd(b,a%b);
+}
+
 int main(){
     for (int i=1;i<=2020;i++){
         for (int j=1;j<=2020;j++){
-            if (__gcd(i,j)==1){
+            if (__gcd(i,j)==1){ //__gcd()位于algorithm
                 ans++;
             }
         }
