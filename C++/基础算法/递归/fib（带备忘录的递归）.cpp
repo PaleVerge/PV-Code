52281e754551bd227714a1da740e9d9d8dd95eb2
PaleVerge <fnme370679@gmail.com>
2026-03-24 23:17:19 +0800

    记忆化fib

--- C++/基础算法/递归/fib（带备忘录的递归）.cpp
+++ C++/基础算法/递归/fib（带备忘录的递归）.cpp
@@ -1,21 +1,21 @@
+#include <cstring>
 #include <iostream>
 #include <iomanip>
 using namespace std;
 using ll = long long;
 const int N =1e5+9;
 
-ll dp[N];
+ll dp[N];	//备忘录数组
 
 ll fib(int n){
-	if(dp[n])return dp[n]; //带备忘录的递归
-	if(n==1 || n==2){ //if(n <= 2){
-		return 1;
-	}else{
-		return dp[n] = fib(n-1)+fib(n-2);
-	}
-	
+	if(dp[n]!=-1)return dp[n]; //被计算过，直接返回
+
+	if(n==1 || n==2) return 1; //if(n <= 2){
+
+	return dp[n] = fib(n-1)+fib(n-2);	//计算，备忘，返回
 }
 int main(){
+	memset(dp,-1,sizeof(dp));	//初始化备忘录，-1为未被计算过
 	int n;
 	cin>>n;
 
