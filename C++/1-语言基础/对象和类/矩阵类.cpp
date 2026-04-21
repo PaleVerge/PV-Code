b7e92753cbe7d38f5a16fac218a7e1044e08761e
PaleVerge <fnme370679@gmail.com>
2026-04-21 23:44:38 +0800

    课后题-类与对象

--- C++/1-语言基础/对象和类/矩阵类.cpp
+++ C++/1-语言基础/对象和类/矩阵类.cpp
@@ -1,20 +1,42 @@
 //
 // Created by ritno on 2026/4/21.
 //
-//
-// Created by ritno on 2026/4/21.
-//
 #include <iostream>
-#include <iomanip>
 using namespace std;
+
 class matrix{
 private:
-
+    int m,n;
+    int a[10][10];
 public:
-
+    matrix(int x,int y){
+        m=x;n=y;
+        int count=1;
+        for (int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                a[i][j]=count++;
+            }
+        }
+    }
+    int at(int row,int col){
+        return a[row-1][col-1];
+    }
+    int at(int row){
+        return row;
+    }
+    int sum(){
+        int rt=0;
+        for (int i=1;i<=m*n;++i){
+            rt+=i;
+        }
+        return rt;
+    }
 };
 
 int main(){
-
+    int rowNum,colNum;
+    cin>>rowNum>>colNum;
+    matrix mat(rowNum,colNum);
+    cout<<mat.at(4)<<' '<<mat.at(3,4)<<' '<<mat.sum();
     return 0;
 }
