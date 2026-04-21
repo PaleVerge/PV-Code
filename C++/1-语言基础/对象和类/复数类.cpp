91edd54eb8daf64cca26732076c3a2b63286a63e
PaleVerge <fnme370679@gmail.com>
2026-04-21 23:23:23 +0800

    课后题-类与对象

--- C++/1-语言基础/对象和类/复数类.cpp
+++ C++/1-语言基础/对象和类/复数类.cpp
@@ -1,3 +1,47 @@
 //
 // Created by ritno on 2026/4/21.
 //
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+class complex{
+private:
+    double real;
+    double imag;
+public:
+    complex(double _real=1.0,double _imag=1.0){
+        real=_real;
+        imag=_imag;
+    }
+    void display(){
+        if (imag){
+            cout<<fixed<<setprecision(2)<<noshowpos<<real<<showpos<<imag<<'i';
+        }else{
+            cout<<fixed<<setprecision(2)<<noshowpos<<real;
+        }
+
+    }
+    void input(){
+        double _real,_imag;
+        cin>>_real>>_imag;
+        real=_real;
+        imag=_imag;
+    }
+    double magnitude(){
+        return sqrt(real*real+imag*imag);
+    }
+};
+int main(){
+    complex val1,val2,val3(1.5,2.8);
+    val1.input();
+    val1.display();
+    cout<<"  "<<noshowpos<<fixed<<setprecision(2)<<val1.magnitude()<<endl;
+    val2.display();
+    cout<<"  "<<noshowpos<<val2.magnitude()<<endl;
+    val3.display();
+    cout<<"  "<<noshowpos<<val3.magnitude()<<endl;
+    return 0;
+}
--- C++/1-语言基础/对象和类/矩阵类.cpp
+++ C++/1-语言基础/对象和类/矩阵类.cpp
@@ -1,3 +1,20 @@
 //
 // Created by ritno on 2026/4/21.
 //
+//
+// Created by ritno on 2026/4/21.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class matrix{
+private:
+
+public:
+
+};
+
+int main(){
+
+    return 0;
+}
