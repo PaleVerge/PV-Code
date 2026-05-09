6d39aa0fc9d97240a65d563a9e1e2d50fca360e8
PaleVerge <fnme370679@gmail.com>
2026-05-09 22:22:36 +0800

    快速幂

--- C++/7-数学/数论/快速幂.cpp
+++ C++/7-数学/数论/快速幂.cpp
@@ -16,7 +16,8 @@ ll qmi(ll a,ll b,ll p){
 int main(){
     using std::cin,std::cout,std::endl;
     int a,b;
+    int p=10000;
     cout<<"请输入底数和指数:";
     cin>>a>>b;
-    cout<<a<<"的"<<b<<"次方为:"<<qmi(a,b)<<endl;
+    cout<<a<<"的"<<b<<"次方为:"<<qmi(a,b,p)<<endl;
 }
