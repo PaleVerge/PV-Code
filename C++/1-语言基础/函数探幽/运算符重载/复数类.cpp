fe7abe346054753999b59e59dd7a817c558dbf5f
PaleVerge <fnme370679@gmail.com>
2026-05-13 19:55:50 +0800

    复数类

---
+++ C++/1-语言基础/函数探幽/运算符重载/复数类.cpp
@@ -0,0 +1,98 @@
+//
+// Created by ritno on 2026/5/11.
+//
+//
+// Created by ritno on 2026/5/11.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class complex{
+private:
+    double real;
+    double imag;
+    double mod(double _real,double _imag){
+        return _real*_real+_imag*_imag;
+    }
+public:
+    friend istream& operator>>(istream& is,complex &m){
+        char pos,i;
+        cin>>m.real>>pos>>m.imag>>i;
+        if (pos=='-') m.imag=-m.imag;
+        return is;
+    }
+    friend ostream& operator<<(ostream& os,complex &m){
+        if (m.imag==0)
+            os<<fixed<<setprecision(2)<<m.real<<endl;
+        else
+            os<<fixed<<setprecision(2)<<m.real<<showpos<<m.imag<<'i'<<endl;
+        return os;
+    }
+    void setReal(double realVal){
+        real=realVal;
+    }
+    void setImag(double imagVal){
+        imag=imagVal;
+    }
+    double getReal(double realVal){
+        return real;
+    }
+    double getImag(double imagVal){
+        return imag;
+    }
+    complex operator+(complex c){
+        complex tmp;
+        tmp.real=real+c.real;
+        tmp.imag=imag+c.imag;
+        return tmp;
+    }
+    complex operator-(complex c){
+        complex tmp;
+        tmp.real=real-c.real;
+        tmp.imag=imag-c.imag;
+        return tmp;
+    }
+    bool operator>(complex c){
+        if (mod(real,imag)>mod(c.real,c.imag)){
+            return true;
+        }
+        return false;
+    }
+    bool operator<(complex c){
+        if (mod(real,imag)<mod(c.real,c.imag)){
+            return true;
+        }
+        return false;
+    }
+    bool operator==(complex c){
+        return (real==c.real&&imag==c.imag)?true:false;
+    }
+    bool operator!=(complex c){
+        return ((*this)==c)?false:true;
+    }
+
+};
+int main()
+{
+    complex c1, c2, c3, c4, c5 ;
+    cin >> c1 >> c2 ;
+    c3 = c1 + c2 ;
+    c4 = c1 - c2 ;
+    if ( c3 != c4 ){
+        if (c3 < c4){
+            cout << "c3 is less than c4" ;
+            c5 = c4 - c3 ;
+        }
+        if ( c3 > c4 ){
+            cout << "c3 is greater than c4" ;
+            c5 = c3 - c4 ;
+        }
+    }
+    if ( c3 == c4 ){
+        cout << "c3 and c4 are equal";
+        c5 = c4 ;
+    }
+    cout<<endl;
+    cout << c5 ;
+    return 0;
+}
