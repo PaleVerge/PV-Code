b1dcdf6e671c77d32558a296681567812c399685
PaleVerge <fnme370679@gmail.com>
2026-05-19 23:00:00 +0800

    小球反弹

--- C++/2-基础算法/枚举/小球反弹.cpp
+++ C++/2-基础算法/枚举/小球反弹.cpp
@@ -1,3 +1,18 @@
 //
 // Created by ritno on 2026/5/17.
 //
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main(){
+    const int width =343720,height=233333;
+    int dx=15,dy=17;
+    for (int t=1;;t++){
+        if (dx*t%(width*2)==0 && dy*t%(height*2)==0){
+            cout<<fixed<<setprecision(2)<<hypot(dx*t,dy*t);
+            break;
+        }
+    }
+    return 0;
+}
