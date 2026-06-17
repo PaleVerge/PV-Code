3bcf780237001d0d72fe0f1af7ee660557eef8c7
PaleVerge <fnme370679@gmail.com>
2026-06-17 10:45:07 +0800

    日期统计完成

--- C++/2-基础算法/枚举/日期统计.cpp
+++ C++/2-基础算法/枚举/日期统计.cpp
@@ -9,16 +9,18 @@ using namespace std;
 int a[100]={5,6,8,6,9,1,6,1,2,4,9,1,9,8,2,3,6,4,7,7,5,9,5,0,3,8,7,5,8,1,5,8,6,1,8,3,0,3,7,9,2,7,0,5,8,8,5,7,0,9,9,1,9,4,4,6,8,6,3,3,
 8,5,1,6,3,4,6,7,0,7,8,2,7,6,8,9,5,6,5,6,1,4,0,1,0,0,9,4,8,0,9,1,2,8,5,0,2,5,3,3};
 
-int dmoon[12]={31,28,31,30,31,30,31,31,30,31,30,31};
-
-bool isdate(vector<int> vec){
-    if (vec[0]!=2 || vec[1]!=0 || vec[2]!=2 ||vec[3]!=3)return false;
-    int day=vec[6]*10+vec[7];
-    int moon=vec[4]*10+vec[5];
-    if (moon<1||moon>12)return false;
-
-    if (day>0&&day<=dmoon[moon-1])
-        return true;
+vector<int> dayInMonth={0,31,28,31,30,31,30,31,31,30,31,30,31};
+//子序列匹配
+bool submatch(vector<int> target){
+    int idx=0;
+    for (int i=0;i<100;i++){
+        if (a[i]==target[idx]){
+            idx++;
+            if (idx==target.size()){
+                return true;
+            }
+        }
+    }
     return false;
 }
 int main(){
@@ -28,14 +30,14 @@ int main(){
     //    cout<<a<<',';
     // }
     vector<int> vec;
-    for (int i=0;i<92;++i){
-        for (int j=i;j<i+8;j++){
-            vec.push_back(a[j]);
+    //枚举每个日期
+    for (int i=1;i<=12;++i){
+        for (int j=1;j<=dayInMonth[i];++j){
+            vector<int> target={2,0,2,3,i/10,i%10,j/10,j%10};
+            if (submatch(target)){
+                cnt++;
+            }
         }
-        if (isdate(vec)){
-            cnt++;
-        }
-        vec.clear();
     }
     cout<<cnt<<endl;
     return 0;
