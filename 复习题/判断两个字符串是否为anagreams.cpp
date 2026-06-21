9f9354631b44a15ed4080ecc7c5bb9885c444631
PaleVerge <fnme370679@gmail.com>
2026-06-21 23:56:30 +0800

    判断anagreams

--- 复习题/判断两个字符串是否为anagreams.cpp
+++ 复习题/判断两个字符串是否为anagreams.cpp
@@ -2,12 +2,24 @@
 // Created by whip on 2026/6/21.
 //
 #include <iostream>
-#include <iomanip>
 #include <string>
-
 using namespace std;
 
 int main(){
-
+    string s1,s2;
+    cin>>s1>>s2;
+    if (s1.size()!=s2.size()){
+        cout<<"no";
+        return 0;
+    }
+    for (int i=0;i<s1.size();++i){
+        size_t pos=s2.find(s1[i]);
+        if (pos==string::npos){
+            cout<<"no";
+            return 0;
+        }
+        s2.erase(pos,1);
+    }
+    cout<<"yes";
     return 0;
 }
