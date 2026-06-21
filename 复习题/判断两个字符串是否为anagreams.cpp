300f5137b4fe067c05e32ef32329eb56af5da637
PaleVerge <fnme370679@gmail.com>
2026-06-22 00:03:49 +0800

    判断anagreams

--- 复习题/判断两个字符串是否为anagreams.cpp
+++ 复习题/判断两个字符串是否为anagreams.cpp
@@ -23,3 +23,27 @@ int main(){
     cout<<"yes";
     return 0;
 }
+
+//
+// Created by whip on 2026/5/27.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    for (int i=0;i<s1.size();++i){
+        if (s2.find(s1[i])==string::npos){
+            cout<<"no";
+            return 0;
+        }
+        s2.erase(s2.find(s1[i]),1);
+        cout<<s2<<endl;
+    }
+    if (s2.empty()){
+        cout<<"yes";
+    }
+    return 0;
+}
