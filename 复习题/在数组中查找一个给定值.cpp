0c296fd57359903c1af704e5cc2ba8a9b49b787b
PaleVerge <fnme370679@gmail.com>
2026-06-23 11:26:14 +0800

    数组排序

--- 复习题/在数组中查找一个给定值.cpp
+++ 复习题/在数组中查找一个给定值.cpp
@@ -1,1 +1,30 @@
-v
+//
+// Created by whip on 2026/6/21.
+//
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+int a[20];
+void search(int n,int s,int &idx){
+    for (int i=0;i<n;++i){
+        if (a[i]==s){
+            idx=i;
+            return ;
+        }
+    }
+    idx=-1;
+    return ;
+}
+int main(){
+    int n;cin>>n;
+    int idx=-1;
+    for (int i=0;i<n;++i){
+        cin>>a[i];
+    }
+    int s;cin>>s;
+    search(n,s,idx);
+    cout<<idx;
+
+}
--- 复习题/数组排序.cpp
+++ 复习题/数组排序.cpp
@@ -6,8 +6,47 @@
 #include <string>
 
 using namespace std;
-
+void _sort(int a[],int n){
+    for (int i=0;i<n-1;i++){
+        for (int j=0;j<n-1-i;j++){
+            if (a[j]>a[j+1]){
+                int tmp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=tmp;
+            }
+        }
+    }
+}
+void _sort(string a[],int n){
+    for (int i=0;i<n-1;i++){
+        for (int j=0;j<n-1-i;j++){
+            if (a[j]>a[j+1]){
+                string tmp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=tmp;
+            }
+        }
+    }
+}
 int main(){
-
+    int n1,n2,a[20];
+    string b[20];
+    cin>>n1;
+    for (int i=0;i<n1;++i){
+        cin>>a[i];
+    }
+    cin>>n2;
+    for (int i=0;i<n2;++i){
+        cin>>b[i];
+    }
+    _sort(a,n1);
+    _sort(b,n2);
+    for (int i=0;i<n1;++i){
+        cout<<a[i]<<' ';
+    }
+    cout<<endl;
+    for (int i=0;i<n2;++i){
+        cout<<b[i]<<' ';
+    }
     return 0;
 }
