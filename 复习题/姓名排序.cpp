19d95b61e9eb99704fde4f4822f75ede8715f3be
PaleVerge <fnme370679@gmail.com>
2026-06-23 10:59:17 +0800

    姓名排序

--- 复习题/姓名排序.cpp
+++ 复习题/姓名排序.cpp
@@ -4,10 +4,20 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-
+    vector<string> name;
+    int n;cin>>n;
+    while (n--){
+        string s;cin>>s;
+        name.push_back(s);
+    }
+    sort(name.begin(),name.end());
+    for (int i=0;i<name.size();++i){
+        cout<<((i==name.size()-1)?name[i]:name[i]+",");
+    }
     return 0;
 }
