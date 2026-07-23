1330a5ea5da509e0b1da2ab1e67e9453f6838987
PaleVerge <fnme370679@gmail.com>
2026-07-23 19:48:57 +0800

    整数排序（贪心）

--- 整除排序.cpp
+++ 整除排序.cpp
@@ -1,34 +1,31 @@
 //
 // Created by whip on 2026/7/19.
 //
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
 using namespace std;
+const int N = 2e5+10;
+const int INF = 1e12+10;
+const int MOD = 1e9+7;
+void init(){
 
-int main(){
+}
+void solve(){
+    int n;cin>>n;
+    int ans=0;
+    for (int i=1;i<=n;i++){
+        int x;cin>>x;
+        if (x%(ans+1)==0) ans++;
+    }
+    cout<<ans<<endl;
+}
+signed main(){
+    init();
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
     int t;cin>>t;
     while (t--){
-         int n;cin>>n;
-        int a[n+1];
-        for (int i=1;i<=n;i++){
-            cin>>a[i];
-        }
-        int max_len=0;
-        for (int i=1;i<=n;i++){
-            int len=0;
-            int k=1;
-            for (int j=i;j<=n;j++){
-                if (a[j]%k==0){
-                    len++;
-                    k++;
-                }
-            }
-            if (len>max_len){
-                max_len=len;
-            }
-        }
-        cout<<max_len<<'\n';
+        solve();
     }
-
     return 0;
 }
