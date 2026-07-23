9658ba4c5b279eb85b29296ca627e7cdb46085a5
PaleVerge <fnme370679@gmail.com>
2026-07-23 19:22:22 +0800

    数组重拍（补）

--- 数组重排.cpp
+++ 数组重排.cpp
@@ -1,14 +1,26 @@
 //
 // Created by whip on 2026/7/19.
 //
-#include <iostream>
+#include <bits/stdc++.h>
+#define ll  long long
+
+#define endl '\n'
 
 using namespace std;
+
+const int N=2e5+10;
+const int INF=1e12+10;
+const int MOD=1e9+7;
+
 int main(){
-    int a[3];
+    vector<int> v(3);
     for (int i=0;i<3;i++){
-        cin>>a[i];
+        cin>>v[i];
     }
-    cout<<a[2]<<' '<<a[0]<<' '<<a[1];
+    for (int i=0;i<3;i++){
+        cout<<v[(i+1)%3]<<' ';//每个数字往后移动一位
+        //数组重排且和原来每位都不同
+    }
+    cout<<endl;
     return 0;
 }
