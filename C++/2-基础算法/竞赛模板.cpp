9e9b45ed6d11da0fb63f5e32d6f389788d6f09d7
PaleVerge <fnme370679@gmail.com>
2026-07-23 19:18:34 +0800

    一个模板

---
+++ C++/2-基础算法/竞赛模板.cpp
@@ -0,0 +1,31 @@
+//
+// Created by whip on 2026/7/23.
+//
+//
+// Created by whip on 2026/7/19.
+//
+#include <bits/stdc++.h>
+#define ll  long long
+
+#define endl '\n'
+
+using namespace std;
+
+const int N=2e5+10;
+const int INF=1e12+10;
+const int MOD=1e9+7;
+void init(){
+
+}
+void solve(){
+
+}
+int main(){
+    init();
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    int t=1;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
